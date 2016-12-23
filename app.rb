31703e0b9b63c259a8330e9e5cacb0cad337ea26
Jonathan Thom <jonathan.thom1990@gmail.com>
2016-12-23 12:43:09 -0800

    Will find elapsed time, just not routing correctly

--- app.rb
+++ app.rb
@@ -1,18 +1,22 @@
 require("bundler/setup")
 Bundler.require(:default)
 require('pry')
+require('time')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get('/') do
+  @start_time = Time.now
   @word = Sentence.all.sample.sentence
   erb(:index)
 end
 
 post('/') do
+  start_time = Time.parse(params['start_time'])
   guess = params['guess']
   @word = params['word']
   if @word == guess
+    @elapsed = Time.now - start_time
     redirect('/')
   else
     @word = @word
